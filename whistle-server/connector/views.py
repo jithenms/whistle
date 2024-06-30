6702a9eb07a62567e3a441cf8de6935bd8e655d3
Jithen Shriyan <shriyanjithen@gmail.com>
2024-06-29 23:14:44 -0400

    feat: twilio and sendgrid connectors

--- whistle-server/connector/views.py
+++ whistle-server/connector/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from authn.authentication import ServerAuthentication
+from connector.models import Twilio, Sendgrid
+from connector.serializers import TwilioSerializer, SendgridSerializer
+
+
+class TwilioViewSet(ModelViewSet):
+    queryset = Twilio.objects.all()
+    serializer_class = TwilioSerializer
+    authentication_classes = [ServerAuthentication]
+
+    def get_queryset(self):
+        return self.queryset.filter(account=self.request.user)
+
+
+class SendgridViewSet(ModelViewSet):
+    queryset = Sendgrid.objects.all()
+    serializer_class = SendgridSerializer
+    authentication_classes = [ServerAuthentication]
+
+    def get_queryset(self):
+        return self.queryset.filter(account=self.request.user)

30eee2a30c62bbd0ee8c3bfcca379c19778eefea
Arman <arman.kumaraswamy@gmail.com>
2024-07-06 19:21:28 -0700

    feat(web, ui): Added NotificationInbox pin animation for landing page

---
+++ web/src/app/(landing)/page.tsx
@@ -0,0 +1,89 @@
+import { PinContainer } from "../../components/ui/3d-pin";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "../../components/ui/avatar";
+import { CircleAlert } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="flex bg-zinc-950 items-center justify-center w-screen h-screen">
+      <PinContainer title="<NotificationInbox />">
+        <div className="bg-white flex flex-col border rounded-xl min-w-max">
+          <div className="flex px-4 pt-4 justify-between">
+            <p className="font-semibold">Notifications</p>
+            <p className="text-gray-500 text-sm">Mark all as read</p>
+          </div>
+          <div className="flex gap-36 border-t items-center justify-between bg-gray-100 mt-4 px-4 py-3">
+            <div className="flex gap-2">
+              <Avatar>
+                <AvatarImage src="https://github.com/jithenms.png" />
+                <AvatarFallback>JS</AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="text-sm text-gray-500">
+                  <span className="font-semibold text-black">@jithenms</span>{" "}
+                  sent you a message
+                </p>
+                <p className="text-xs text-gray-600">28 mins ago</p>
+              </div>
+            </div>
+            <div>
+              <div className="w-2 h-2 bg-blue-500 rounded-full" />
+            </div>
+          </div>
+          <div className="flex border-t items-center justify-between bg-gray-100 px-4 py-3">
+            <div className="flex gap-2">
+              <Avatar>
+                <AvatarImage src="https://github.com/armans-code.png" />
+                <AvatarFallback>AK</AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="text-sm text-gray-500">
+                  <span className="font-semibold text-black">@armank</span>{" "}
+                  added you to{" "}
+                  <span className="font-semibold text-black">
+                    Designers Groupchat
+                  </span>
+                </p>
+                <p className="text-xs text-gray-600">2 hours ago</p>
+              </div>
+            </div>
+            <div>
+              <div className="w-2 h-2 bg-blue-500 rounded-full" />
+            </div>
+          </div>
+          <div className="flex border-t items-center justify-between px-4 py-3 rounded-b-xl">
+            <div className="flex gap-2">
+              <div className="flex items-center justify-center h-10 w-10 overflow-hidden">
+                <CircleAlert />
+              </div>
+              <div>
+                <p className="text-sm text-gray-700">
+                  Your password has been changed
+                </p>
+                <p className="text-xs text-gray-600">7 hours ago</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex border-t items-center justify-between px-4 py-3">
+            <div className="flex gap-2">
+              <Avatar>
+                <AvatarImage src="https://github.com/person.png" />
+                <AvatarFallback>PE</AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="text-sm text-gray-500">
+                  <span className="font-semibold text-black">@person</span>{" "}
+                  mentioned you in a post
+                </p>
+                <p className="text-xs text-gray-600">Yesterday</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </PinContainer>
+    </div>
+  );
+}
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,20 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <header>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
           <main>{children}</main>
         </body>
       </html>
-    </ClerkProvider>
   );
 }

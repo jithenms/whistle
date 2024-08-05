76250bbef8c0d3b95258da811827af1493d2d3e8
Jithen Shriyan <shriyanjithen@gmail.com>
2024-08-04 21:42:55 -0400

    feat: default db settings

--- whistle/whistle/settings.py
+++ whistle/whistle/settings.py
@@ -150,8 +150,8 @@ ASGI_APPLICATION = "whistle.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("SQL_DATABASE", "whistle_db"),
         "USER": os.environ.get("SQL_USER", "postgres"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "postgres"),
         "HOST": os.environ.get("SQL_HOST", "127.0.0.1"),
